188fb3ae519b6fbcf412d9151854a819006f2de1
Nandini_Chandel <nandinichandel107@gmail.com>
2025-04-18 15:26:50 +0000

    Conversions

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,28 @@
+let score=33
+console.log(typeof (score));
+
+let score2="33abc"
+console.log(typeof (score2))
+
+let valInNum=Number(score2)
+console.log(typeof (valInNum))
+console.log(valInNum)
+
+
+//"33"=>33
+//"33abc"=>NaN
+//undefined=>NaN
+//null=>0
+//true=>1 ; false=>0
+
+let loggedIn=1
+let boolLoggedIn=Boolean(loggedIn)
+
+console.log(boolLoggedIn)
+
+//anything other than 1 and empty string return true
+// 1=> true;
+//0=>false; ""=>false
+
+
+
