ba04e792e3ae0118960f0e359a96dc8819997261
Nandini_Chandel <nandinichandel107@gmail.com>
2025-04-18 14:55:50 +0000

    Datatypes

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,17 @@
+const accountId=144553
+let accountEmail="nandini1122@gmail.com"
+var accountPassword="123key"
+accountCity="Bengluru"
+let accountState;
+
+// accountId=289320 //NOT ALLOWED
+
+/*
+Prefer Not to use var
+because of issue in Block Space and Functional Space
+*/
+
+accountEmail="abc@itsy.com"
+accountPassword=789742
+accountCity="Ahmedabad"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js as newer version of javascript
+
+//alert(3+3) //we are using nodejs not browser(the engine of js)
+
+let name="Nandini_Chandel";
+let age=20
+let isLoggedIn=true
+
+//number=>2 to the power 53
+//bigint
+//string=>
+//boolean=>true/false
+//null=>      ***Representation of Empty Value***  standalone value 
+//undefined=> ***Not defined yet***
+//symbol=>    ***Used to find uniqueness***
+
+
+console.log(typeof name);
+console.log(typeof null); //==>Object
+console.log(typeof undefined); //==>undefined
+
